45659d805f31db883b6e86f7a75aced06d2662c7
richar2005 <1431574@senati.pe>
2023-09-29 11:25:13 -0500

    primero

---
+++ holamundo.js
@@ -0,0 +1,14 @@
+// Maneras de hacer un "Hola Mundo" en JavaScript
+
+//En primer lugar tenenemos la más utilizada y funciona mostrando una aleta.
+// CODIGO#1
+alert("Hola Mundo");
+
+/*En segundo lugar podemos hacer que el mensaje se muestre en la consola o terminal */
+// CODIGO#2
+console.log("Hola Mundo");
+//Abre la consola dando click derecho y después dale a Inspeccionar Elemento
+
+
+//Por último lugar tenemos una manera de imprimir "Hola Mundo"
+//Se trata de una forma en la que interactuamos con HTML (Si sabes HTML te interesará)
